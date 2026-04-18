16c149c8bc5ba0146f66a8d00fba4bb78c00ab9e
wakeup-jarvis <138219616+wakeup-jarvis@users.noreply.github.com>
2026-04-18 19:23:22 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def calculator(a,b,operator):
+    if operator=="-":
+        return a-b
+    elif operator=="+":
+        return a+b
+    elif operator=="*":
+        return a*b
+    elif operator=="/":
+        return a/b
+    else:
+        return "Invalid Input"
+cal = calculator(5,8,"a")
+print(cal)
